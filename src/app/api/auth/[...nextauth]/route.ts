fcf77891242aa701988f4ce56686c068c8ff5788
Andres Julian <andres.julian@onegolive.com>
2026-04-12 16:00:14 +0200

    feat: setup next-auth with github provider

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,26 @@
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+const handler = NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+      authorization: { params: { scope: 'repo' } },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }: any) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
